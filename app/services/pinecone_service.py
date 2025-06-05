f14a6f0cd25587eca0c1b691beb51d7cef0faa96
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2025-06-05 16:14:00 +0300

    set up pinecone

--- app/services/pinecone_service.py
+++ app/services/pinecone_service.py
@@ -1,15 +1,15 @@
-# import os 
-# from pinecone import Pinecone, ServerlessSpec
-# from dotenv import load_dotenv 
+import os 
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv 
  
-# load_dotenv() 
+load_dotenv() 
  
-# pinecone = Pinecone(
-#     api_key=os.environ.get("PINECONE_API_KEY")
-# )
+pinecone = Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY")
+)
 
-from langchain.embeddings.openai import OpenAIEmbeddings 
-from langchain.vectorstores import Pinecone as PineconeVectorStore 
+# from langchain.embeddings.openai import OpenAIEmbeddings 
+# from langchain.vectorstores import Pinecone as PineconeVectorStore 
  
-embedding = OpenAIEmbeddings() 
-vectorstore = PineconeVectorStore.from_documents(docs, embedding, index_name=index_name)  
+# embedding = OpenAIEmbeddings() 
+# vectorstore = PineconeVectorStore.from_documents(docs, embedding, index_name=index_name)  
