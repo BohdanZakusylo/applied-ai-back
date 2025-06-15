87c481f9f7b3d00059ac5fa5da15ae1430f05257
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2025-06-15 23:33:19 +0200

    Add request authentication to call feedback endpoint

--- app/routers/feedback.py
+++ app/routers/feedback.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from app.orm.feedback import Feedback
 from app.models.feedback import FeedbackCreate
 from app.services.feedback_service import FeedbackService
+from app.dependencies import get_current_user
 
 router = APIRouter()
 
 @router.post("/feedback", status_code=status.HTTP_201_CREATED)
-def create_feedback(feedback: FeedbackCreate):
-    FeedbackService.submit_feedback(feedback)
-    return {"message": "Feedback submitted successfully."}
+async def create_feedback(feedback: FeedbackCreate, current_user: str = Depends(get_current_user)):
+    if (current_user):
+      FeedbackService.submit_feedback(feedback)
+      return {"message": "Feedback submitted successfully."}
